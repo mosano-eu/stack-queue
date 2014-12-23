b18dc4c2a33a953cfdb629a6db73c485419edb22
José Moreira <jmoreira@microdual.com>
2014-12-23 19:22:06 +0000

    Version: 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
 stack-queue
 ===========
 
-stackable objects compatible with promises
+Execution stacks compatible with promises
+
+## Installation
+
+```bash
+npm install --save stack-queue
+```
+
+## Usage
+
+Have you ever used [express](/strongloop/express) or [connect](/strongloop/connect)?
+
+If so, you should know how they share they stack with `function ( req, res, next )`.
+
+This module allows you to apply a **nextable queue** method in any circunstance,
+and compatible with promises!
+
+Lets see some examples:
+
+### Connect Example
+
+This is how you would implement a [connect](/strongloop/connect)-like example:
+
+```js
+
+var http = require( 'http' ),
+    Stack = require( 'stack-queue' );
+
+var stack = new Stack(),
+    server = http.createServer( stack.dispatch.bind( stack ) ).listen( 80 );
+
+// Now you should be able to stack things on request
+stack.queue( function ( req, res, next ) {
+    req.foo = 'bar';
+
+    next();
+});
+
+// You can even chain stack.queue calls
+stack
+    .queue(function ( req, res ) {
+        res.statusCode = 200;
+
+        return somePromisedMethod();
+    })
+    .queue(function ( req, res, next ) {
+        res.write( 'hey' );
+
+        next();
+    });
+
+// Or provide an array with functions
+stack.queue([
+    function () { return true; },
+    function () { throw new Error("WOW"); }
+]);
+
+```
+
+### Do you have more examples?
+
+Please **Pull Request** them! :)
---
+++ test/stack.js
@@ -0,0 +1,124 @@
+var Stack = require( '../index' ),
+
+    sinon = require( 'sinon' ),
+    chai = require( 'chai' ),
+    expect = chai.expect;
+
+describe( "Stack", function () {
+    var stack;
+
+    beforeEach(function () {
+        stack = new Stack();
+    });
+
+    it( "should queue a function", function () {
+        stack.queue( sinon.stub() );
+    });
+
+    it( "should queue from an array", function () {
+
+        var array = [
+            sinon.stub(),
+            sinon.stub(),
+            sinon.stub(),
+            sinon.stub(),
+        ];
+
+        stack.queue( array );
+
+        expect( stack.length ).to.be.equal( array.length );
+
+    });
+
+    it( "shouldn't queue a string", function () {
+        expect(function () {
+            stack.queue( "yehe" );
+        }).to.throw( Error );
+    });
+
+    it( "should capture errors to promise, from next", function () {
+
+        stack.queue(function ( next ) {
+            next( new Error( "test" ) );
+        })
+
+        return stack.dispatch()
+            .then(function () {
+                throw new Error( "stack should not fulfill this promise" );
+            }, function ( err ) {
+                expect( err.message ).to.equal( "test" );
+            });
+        });
+
+    it( "should capture errors to promise, from second next", function () {
+
+        stack.queue( sinon.stub().callsArg( 0 ) );
+
+        stack.queue(function ( next ) {
+            next( new Error( "test" ) );
+        });
+
+        return stack.dispatch()
+            .then(function () {
+                throw new Error( "stack should not fulfill this promise" );
+            }, function ( err ) {
+                expect( err.message ).to.equal( "test" );
+            });
+    });
+
+    it( "should capture errors to promise, from internal throw", function () {
+
+        stack.queue( sinon.stub().throws( new Error( "test" ) ) );
+
+        return stack.dispatch()
+            .then(function () {
+                throw new Error( "stack should not fulfill this promise" );
+            }, function ( err ) {
+                expect( err.message ).to.equal( "test" );
+            });
+
+    });
+
+    it( "should transport arguments given to dispatch", function () {
+
+        var args = [
+            'lol',
+            'lola'
+        ];
+
+        stack.queue(function ( first, second, next ) {
+            expect( first ).to.equal( args[0] );
+            expect( second ).to.equal( args[1] );
+
+            next();
+        });
+
+        return stack.dispatch( args[0], args[1] );
+    });
+
+    it( "should continue stack with some positive return instead of `next`", function () {
+
+        stack.queue(function ( next ) {
+            return 'yolo';
+        });
+
+        return stack.dispatch()
+            .then(function ( value ) {
+                expect( value ).to.equal( 'yolo' );
+            });
+    })
+
+    // Examples:
+
+    describe( "small http simulation", function () {
+        var req, res;
+
+        beforeEach(function () {
+            req = 'request';
+            res = 'response';
+        });
+
+        // TODO
+    });
+
+});
